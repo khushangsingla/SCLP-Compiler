a5c3ae7c806e110032e71cde150eacb9735915fe
Hrishikesh <hrishijedhe06@gmail.com>
2024-04-15 18:47:52 +0530

    new (my)testcase added

---
+++ testing/example-programs/my_tests/test24.c
@@ -0,0 +1,27 @@
+int x,y,z;
+float k(int uui, int y)
+{
+	
+	int x;
+	x = 5>4 ? 5 : 4;
+	return 0.2;
+}
+int g()
+{
+	return y;
+}
+void lmao2()
+{
+	int x;
+	x = 5;
+}
+bool main()
+{
+	bool lmao;
+	float zz;
+	lmao2();
+	zz = k(x+5*4, y);
+	// lmao = main();
+	// zz = g();
+	return lmao;
+}
