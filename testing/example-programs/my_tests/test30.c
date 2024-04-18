2637361e16d50c060f8e9907fe45611d251ef472
Khushang Singla <khushangsingla@gmail.com>
2024-04-18 18:17:51 +0530

    Added test cases for undefined functions

---
+++ testing/example-programs/my_tests/test30.c
@@ -0,0 +1,47 @@
+string ss;
+float c;
+int aaaa;
+bool sjh;
+int fn(int x, string y, float z, int not)
+{
+	int gn;
+	print x;
+	print y;
+	print z;
+	print not;
+	print gn;
+
+	return 1;
+}
+int kn();
+void gn();
+void main(){
+	int x;
+	float y;
+	string z;
+	bool a;
+	// x = 1;
+	// a = y>0.2 || y<0.2 || y == 0.2 || y != 0.2 || y>=0.2 || y<=0.2;
+	// x= -x + 1 * x / x;
+	// y= -y + 1.0 * y / y;
+	
+	x = a ? 1 : 2;
+	x = fn(1, "abc", 1.0, 1);
+	x = fn(x, z, y, x);
+	// if(a){
+	// 	z = "abc";
+	// }
+	// else{
+	// 	y = 3.0;
+	// }
+	// print x;
+	// print y;
+	// print z;
+	// read x;
+	print aaaa;
+}
+
+gn(){
+	int gn;
+	print "gn";
+}
